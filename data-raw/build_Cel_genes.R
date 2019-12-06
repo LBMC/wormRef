ed9c955a680bccba7aa48563554b03e53f72e886
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-12-06 15:34:59 +0100

    Added script to download the gene ids and lenghts from the biomart

---
+++ data-raw/build_Cel_genes.R
@@ -0,0 +1,16 @@
+requireNamespace('biomaRt', quietly = T)
+
+# get gene length info from the biomart
+mart <- biomaRt::useMart("ensembl", dataset = "celegans_gene_ensembl", version = "Ensembl Genes 98")
+
+Cel_genes <- biomaRt::getBM(attributes = c("ensembl_gene_id",        # WB gene ids
+                                           "ensembl_transcript_id",  # Sequence names
+                                           "external_gene_name",     # Public names
+                                           "transcript_length"),     # Transcript lengths
+                            mart = mart)
+
+colnames(Cel_genes) <- c("wb_id", "transcript_name", "public_name", "transcript_length")
+
+# save object to data
+save('Cel_genes', file = "../data/Cel_genes.RData")
+rm(mart, Cel_genes)
