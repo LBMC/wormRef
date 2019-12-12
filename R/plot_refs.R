70405097c643de2bee135e4dcd3f7984c86e5aad
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-12-12 17:27:46 +0100

    Started the plot_refs function

---
+++ R/plot_refs.R
@@ -0,0 +1,28 @@
+
+plot_refs_Cel <- function(){
+  if(!requireNamespace("ggplot2")){
+    message("Install ggplot2 to see this graph !")
+  }
+  lbs <- data.frame(tunit=unique(Cel_devstages$devstages$tunit), lab = c("Embryo", "Post-Hatching"))
+  
+  eb <- ggplot2::element_blank()
+    
+  ggplot2::ggplot(data = Cel_devstages$datasets, mapping = ggplot2::aes(x = tstart, y = name, xend = tend,)) +
+    ggplot2::geom_segment(data = Cel_devstages$datasets, ggplot2::aes( yend = name, col = name, size = 1.5), show.legend = F) +
+    ggplot2::geom_text(data = Cel_devstages$datasets, ggplot2::aes(col = name, label = name, size = 2), 
+                       fontface = "bold", vjust = "bottom", hjust = "left", nudge_y = .1,
+                       show.legend = F) +
+    ggplot2::geom_text(data = lbs, ggplot2::aes(label = tunit), 
+                       x = Inf, y = -Inf, hjust = 1.5, vjust = -1, inherit.aes = F) +
+    
+    ggplot2::geom_segment(data = Cel_devstages$devstages, ggplot2::aes(size = 1, y = "DevStage", yend = "DevStage"), 
+                          color = rep(c("grey50", "grey90"), length.out = nrow(Cel_devstages$devstages)), show.legend = F) +
+    ggplot2::geom_text(data = Cel_devstages$devstages, ggplot2::aes(y = "DevStage", label = name), size = 3, 
+                       fontface = "bold", vjust = "bottom", hjust = "left", nudge_y = .1,
+                       show.legend = F) +
+    
+    ggplot2::facet_wrap(~tunit, scales = "free", nrow = 2) +
+    ggplot2::theme_classic() + ggplot2::xlab("time") + 
+    ggplot2::theme(strip.background = eb, strip.text = eb, 
+                   axis.title.y = eb, axis.text.y = eb) 
+}
