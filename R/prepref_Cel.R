61e90dabfc631770a53e322b016c544d2a0014d9
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-12-12 15:55:06 +0100

    Added the prep_ref functions for all references

---
+++ R/prepref_Cel.R
@@ -0,0 +1,67 @@
+#' Build Interpolated Gene Expression Reference
+#' 
+#' Builds the PLSR interpolation of the reference dataset
+#' 
+#' @param n.inter passed on to \code{\link[wormAge]{plsr_interpol}}
+#' 
+#' @return The output of \code{\link[wormAge]{plsr_interpol}}
+#' 
+#' @name Cel_prep
+NULL
+
+#' @rdname Cel_prep
+#' @importFrom wormAge plsr_interpol
+#' 
+.prepref_Cel_embryo <- function(n.inter){
+  return(
+    wormAge::plsr_interpol(
+      X = Cel_embryo$g, 
+      time.series = Cel_embryo$p$age, 
+      covar = Cel_embryo$p$cov, 
+      df = Cel_embryo$df,
+      n.inter = n.inter)
+    )
+}
+
+#' @rdname Cel_prep
+#' @importFrom wormAge plsr_interpol
+#'
+.prepref_Cel_larval <- function(n.inter){
+  return(
+    wormAge::plsr_interpol(
+      X = Cel_larval$g, 
+      time.series = Cel_larval$p$age, 
+      covar = Cel_larval$p$cov, 
+      df = Cel_larval$df,
+      n.inter = n.inter)
+  )
+}
+
+#' @rdname Cel_prep
+#' @importFrom wormAge plsr_interpol
+#'
+.prepref_Cel_YA_1 <- function(n.inter){
+  return(
+    wormAge::plsr_interpol(
+      X = Cel_YA_1$g, 
+      time.series = Cel_YA_1$p$age, 
+      covar = Cel_YA_1$p$cov, 
+      df = Cel_YA_1$df,
+      n.inter = n.inter)
+  )
+}
+
+#' @rdname Cel_prep
+#' @importFrom wormAge plsr_interpol
+#'
+.prepref_Cel_YA_2 <- function(n.inter){
+  return(
+    wormAge::plsr_interpol(
+      X = Cel_YA_2$g, 
+      time.series = Cel_YA_2$p$age, 
+      covar = Cel_YA_2$p$cov, 
+      df = Cel_YA_2$df,
+      n.inter = n.inter,
+      tmin = 48) # because of bad quality
+  )
+}
