314f4cbac4cf0083c4b8b0d891b5db09ab6af9e4
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-12-18 13:53:25 +0100

    Added .data$ to plots to remove devtools note

--- R/plot_refs.R
+++ R/plot_refs.R
@@ -9,7 +9,7 @@
 #' 
 #' @export
 #' 
-#' @importFrom utils data
+#' @importFrom rlang .data
 plot_refs_Cel <- function(){
   if(!requireNamespace("ggplot2")){
     message("Install ggplot2 to see this graph !")
@@ -20,21 +20,21 @@ plot_refs_Cel <- function(){
   
   eb <- ggplot2::element_blank()
     
-  ggplot2::ggplot(data = dat$datasets, mapping = ggplot2::aes(x = tstart, y = name, xend = tend,)) +
-    ggplot2::geom_segment(data = dat$datasets, ggplot2::aes( yend = name, col = name, size = 1.5), show.legend = F) +
-    ggplot2::geom_text(data = dat$datasets, ggplot2::aes(col = name, label = name, size = 2), 
+  ggplot2::ggplot(data = dat$datasets, mapping = ggplot2::aes(x = .data$tstart, y = .data$name, xend = .data$tend,)) +
+    ggplot2::geom_segment(data = dat$datasets, ggplot2::aes(yend = .data$name, col = .data$name, size = 1.5), show.legend = F) +
+    ggplot2::geom_text(data = dat$datasets, ggplot2::aes(col = .data$name, label = .data$name, size = 2), 
                        fontface = "bold", vjust = "bottom", hjust = "left", nudge_y = .1,
                        show.legend = F) +
-    ggplot2::geom_text(data = lbs, ggplot2::aes(label = tunit), 
+    ggplot2::geom_text(data = lbs, ggplot2::aes(label = .data$tunit), 
                        x = Inf, y = -Inf, hjust = 1.5, vjust = -1, inherit.aes = F) +
     
     ggplot2::geom_segment(data = dat$devstages, ggplot2::aes(size = 1, y = "DevStage", yend = "DevStage"), 
                           color = rep(c("grey50", "grey90"), length.out = nrow(dat$devstages)), show.legend = F) +
-    ggplot2::geom_text(data = dat$devstages, ggplot2::aes(y = "DevStage", label = name), size = 3, 
+    ggplot2::geom_text(data = dat$devstages, ggplot2::aes(y = "DevStage", label = .data$name), size = 3, 
                        fontface = "bold", vjust = "bottom", hjust = "left", nudge_y = .1,
                        show.legend = F) +
     
-    ggplot2::facet_wrap(~tunit, scales = "free", nrow = 2) +
+    ggplot2::facet_wrap(~.data$tunit, scales = "free", nrow = 2) +
     ggplot2::theme_classic() + ggplot2::xlab("time") + 
     ggplot2::theme(strip.background = eb, strip.text = eb, 
                    axis.title.y = eb, axis.text.y = eb) 
