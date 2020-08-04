deff4db05078a3d47d0c2bea9828aa6d518957a0
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2020-08-04 17:55:28 +0200

    Generated doc file for new reference

---
+++ R/Cel_larv_YA-data.R
@@ -0,0 +1,23 @@
+#' Cel_larv_YA
+#' 
+#' Transcriptomic data from *C. elegans* early larval to young adult samples.
+#' 
+#' One time series is included in this dataset.
+#' 
+#'  - \insertCite{meeuse2020developmental}{wormRef}
+#' 
+#' 
+#' The gene expression matrix is \eqn{log(X + 1)} of quantile-normalized raw data.
+#' 
+#' @docType data
+#' 
+#' @eval data_format()
+#' 
+#' @source \href{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE130811}{GSE130811} 
+#' 
+#' @references
+#'    \insertAllCited{}
+#' 
+#' 
+#' @importFrom Rdpack reprompt
+"Cel_larv_YA"
