31f2ecfb6aba29573974d550efb12171becfa400
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2020-08-04 17:47:22 +0200

    Added g_url to cleanup

--- data-raw/build_Cel_larv_YA.R
+++ data-raw/build_Cel_larv_YA.R
@@ -82,7 +82,7 @@ save('Cel_larv_YA', file = "data/Cel_larv_YA.RData", compress = "xz")
 
 
 file.remove(g_f)
-rm(g, p, g_f, sel, geo_id,
+rm(g, p, g_f, g_url, sel, geo_id,
    m_larv, ndat, r_larv, ae_young,
    nc, pca, lm_y,
    Cel_genes, Cel_larval, Cel_larv_YA)
